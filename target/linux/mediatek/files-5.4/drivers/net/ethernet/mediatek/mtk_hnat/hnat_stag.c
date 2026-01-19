d405244d2ad34d38246d12780a0adc339f4ea5eb
3rd-eyes <dorae0234@gmail.com>
2026-01-19 21:21:08 +0800

    revert

--- target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat_stag.c
+++ target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat_stag.c
@@ -31,7 +31,7 @@ int hnat_dsa_fill_stag(const struct net_device *netdev,
 	struct net_device *ndev;
 	int port_index;
 	u16 dsa_tag;
-	if (hw_path->flags & BIT(DEV_PATH_VLAN))
+	if (hw_path->flags & FLOW_OFFLOAD_PATH_VLAN)
 		ndev = hw_path->dev;
 	else
 		ndev = (struct net_device *)netdev;
--- target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/nf_hnat_mtk.h
+++ target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/nf_hnat_mtk.h
@@ -18,13 +18,6 @@
 #include <linux/netdevice.h>
 #include "../mtk_eth_soc.h"
 
-/* Frame Engine Reset FSM */
-#define MTK_FE_START_RESET	0x2000
-#define MTK_FE_RESET_DONE	0x2001
-#define MTK_WIFI_RESET_DONE	0x2002
-#define MTK_WIFI_CHIP_ONLINE 	0x2003
-#define MTK_WIFI_CHIP_OFFLINE 	0x2004
-#define MTK_FE_RESET_NAT_DONE	0x4001
 #define HNAT_SKB_CB2(__skb) ((struct hnat_skb_cb2 *)&((__skb)->cb[44]))
 struct hnat_skb_cb2 {
 	__u32 magic;
